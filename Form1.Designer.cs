3f2c24edc2f837a15f20fe09c3291eb1dc614a0d
komanda14 <163890822+komanda14@users.noreply.github.com>
2024-03-20 21:05:16 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace elecdnevnik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.enter_text = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btExit = new System.Windows.Forms.Button();
+            this.btEnter = new System.Windows.Forms.Button();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.dateTime = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // enter_text
+            // 
+            this.enter_text.AutoSize = true;
+            this.enter_text.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.enter_text.ForeColor = System.Drawing.Color.Black;
+            this.enter_text.Location = new System.Drawing.Point(0, 88);
+            this.enter_text.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.enter_text.Name = "enter_text";
+            this.enter_text.Size = new System.Drawing.Size(377, 22);
+            this.enter_text.TabIndex = 0;
+            this.enter_text.Text = "Вход в программу электронного дневника";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 147);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Логин";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(158, 183);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Пароль";
+            // 
+            // btExit
+            // 
+            this.btExit.BackColor = System.Drawing.Color.LightCoral;
+            this.btExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btExit.Location = new System.Drawing.Point(308, -1);
+            this.btExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(68, 25);
+            this.btExit.TabIndex = 3;
+            this.btExit.Text = "Выход";
+            this.btExit.UseVisualStyleBackColor = false;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // btEnter
+            // 
+            this.btEnter.BackColor = System.Drawing.Color.LightGreen;
+            this.btEnter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btEnter.Location = new System.Drawing.Point(255, 218);
+            this.btEnter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btEnter.Name = "btEnter";
+            this.btEnter.Size = new System.Drawing.Size(60, 25);
+            this.btEnter.TabIndex = 4;
+            this.btEnter.Text = "Войти";
+            this.btEnter.UseVisualStyleBackColor = false;
+            this.btEnter.Click += new System.EventHandler(this.btEnter_Click);
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(222, 146);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(122, 20);
+            this.usernameTextBox.TabIndex = 5;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(222, 179);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(122, 20);
+            this.passwordTextBox.TabIndex = 6;
+            // 
+            // dateTime
+            // 
+            this.dateTime.CalendarTrailingForeColor = System.Drawing.SystemColors.ControlText;
+            this.dateTime.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dateTime.Enabled = false;
+            this.dateTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTime.Location = new System.Drawing.Point(4, 3);
+            this.dateTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dateTime.Name = "dateTime";
+            this.dateTime.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dateTime.Size = new System.Drawing.Size(76, 20);
+            this.dateTime.TabIndex = 7;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Enabled = false;
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(61, 3);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(65, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::elecdnevnik.Properties.Resources._32730184_dnevnik_raskraska_34;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 121);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(382, 262);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dateTime);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.btEnter);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.enter_text);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label enter_text;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btExit;
+        private System.Windows.Forms.Button btEnter;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.DateTimePicker dateTime;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace elecdnevnik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\redmi\source\Repos\elecdnevnik\elecdnevnik\Database2.mdf;Integrated Security=True");
+
+
+        private void btExit_Click(object sender, EventArgs e)
+        {
+            DialogResult res;
+            res = MessageBox.Show("Вы хотите выйти?", "Выход", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else { this.Show(); }
+        }
+
+        private void btEnter_Click(object sender, EventArgs e)
+        {
+            String username, user_password;
+            username = usernameTextBox.Text;
+            user_password = passwordTextBox.Text;
+            try
+            {
+                String querry = "SELECT * FROM Admin WHERE Login = '" + usernameTextBox.Text + "' AND Pass = '" + passwordTextBox.Text + "'";
+                String querry1 = "SELECT * FROM Teacher WHERE Login = '" + usernameTextBox.Text + "' AND Pass = '" + passwordTextBox.Text + "'";
+                String querry2 = "SELECT * FROM Students WHERE Login = '" + usernameTextBox.Text + "' AND Pass = '" + passwordTextBox.Text + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+                SqlDataAdapter sda1 = new SqlDataAdapter(querry1, conn);
+                SqlDataAdapter sda2 = new SqlDataAdapter(querry2, conn);
+
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                DataTable dt1 = new DataTable();
+                sda1.Fill(dt1);
+
+                DataTable dt2 = new DataTable();
+                sda2.Fill(dt2);
+
+                if (dt.Rows.Count > 0)
+                {
+                    username = usernameTextBox.Text;
+                    user_password = passwordTextBox.Text;
+                    MessageBox.Show("Вы вошли как Админ", "Админ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Menuform form2 = new Menuform();
+                    
+                    form2.Show();
+                    this.Hide();
+
+                    
+                }
+                
+
+                else if (dt1.Rows.Count > 0)
+                {
+                    username = usernameTextBox.Text;
+                    user_password = passwordTextBox.Text;
+                    MessageBox.Show("Вы вошли как Учитель", "Учитель", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Menuform form2 = new Menuform();
+                    form2.Show();
+
+                    this.Hide();
+                }
+                
+
+                else if (dt2.Rows.Count > 0)
+                {
+                    username = usernameTextBox.Text;
+                    user_password = passwordTextBox.Text;
+                    MessageBox.Show("Вы вошли как Студент", "Студент", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Menuform form2 = new Menuform();
+                    form2.Show();
+
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    usernameTextBox.Clear();
+                    passwordTextBox.Clear();
+                    usernameTextBox.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
+
